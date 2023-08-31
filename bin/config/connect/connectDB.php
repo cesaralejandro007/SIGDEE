18d90fbc96ef8391c0032f0a12da59834a6007b7
Cesar <cesaralejandrovides2023@gmail.com>
2023-08-31 14:42:33 -0400

    conexion modificada

--- bin/config/connect/connectDB.php
+++ bin/config/connect/connectDB.php
@@ -4,24 +4,52 @@
 	use config\componentes\configSistema as configSistema;
 	use \PDO;
 
-	 class connectDB {
+	 class connectDB extends configSistema{
 
+		private $puerto;
+		private $usuario;
+		private $password;
+		private $local;
+		private $nameDB;
 		protected $conex;
+		protected $pool = [];
 
 		public function __construct(){
-			$pool = new ConnectionPool();
-			//usar método no estático
-			$this->conex = $pool->getConnection();
-		
-		  }
-		
-		  public function __destruct(){
-			$pool = new ConnectionPool();
-			$pool->release($this->conex);
-		  
-		  }		
+			$this->usuario = parent::_USER_();
+			$this->password = parent::_PASS_();
+			$this->local = parent::_LOCAL_();
+			$this->nameDB = parent::_BD_();
+			$this->conex = $this->getConnection();
+		}
 
-		public function query($sql) {
+		public function __destruct(){
+			$this->release($this->conex);
+		}		
+
+		protected function getConnection(){
+			try {
+				if(count($this->pool) > 0){
+					return array_pop($this->pool);
+				} else {
+					$conn = new PDO("mysql:host={$this->local};dbname={$this->nameDB}", $this->usuario , $this->password); 
+				return $conn;
+				}
+			} catch (PDOException $e) {
+				//manejar error
+				$this->logError($e); 
+			}
+		}
+		
+		protected function release($conn){
+			try {  
+				$this->pool[] = $conn;
+			} catch (Exception $e) {
+				//manejar error
+				$this->logError($e);
+			}
+		}
+
+		protected function query($sql) {
 			try{
 				return $this->conex->query($sql);
 			}catch (PDOException $e) {
@@ -29,45 +57,8 @@
 				die();
 			}
 		}
-	  }
-	  
-	  //Pool de conexiones
-	  
-	  class ConnectionPool  extends configSistema{
+	}
 
-		private $puerto;
-		private $usuario;
-		private $password;
-		private $local;
-		private $nameDB;
-
-		public function __construct(){
-			$this->usuario = parent::_USER_();
-			$this->password = parent::_PASS_();
-			$this->local = parent::_LOCAL_();
-			$this->nameDB = parent::_BD_();
-		}
-
-	    private $pool = [];
-
-		public function getConnection(){
-
-			if(count($this->pool) > 0){
-			  return array_pop($this->pool);
-			} else {
-				$conn = new PDO("mysql:host={$this->local};dbname={$this->nameDB}", $this->usuario , $this->password); 
-			  return $conn;
-			}
-		
-		  }
-	  
-		public function release($conn){
-
-			$this->pool[] = $conn;
-		
-		}
-	  
-	  }
  ?>
 
 
