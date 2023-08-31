663b9fcbb87fb31352e56304e230e9c6d6ff5fe0
Cesar <cesaralejandrovides2023@gmail.com>
2023-08-31 13:17:01 -0400

    pool

--- bin/config/connect/connectDB.php
+++ bin/config/connect/connectDB.php
@@ -4,9 +4,37 @@
 	use config\componentes\configSistema as configSistema;
 	use \PDO;
 
-	class connectDB extends configSistema{
+	 class connectDB {
 
 		protected $conex;
+
+		public function __construct(){
+			$pool = new ConnectionPool();
+			//usar método no estático
+			$this->conex = $pool->getConnection();
+		
+		  }
+		
+		  public function __destruct(){
+			$pool = new ConnectionPool();
+			$pool->release($this->conex);
+		  
+		  }		
+
+		public function query($sql) {
+			try{
+				return $this->conex->query($sql);
+			}catch (PDOException $e) {
+				print "¡Error!: " . $e->getMessage() . "<br/>";
+				die();
+			}
+		}
+	  }
+	  
+	  //Pool de conexiones
+	  
+	  class ConnectionPool  extends configSistema{
+
 		private $puerto;
 		private $usuario;
 		private $password;
@@ -14,21 +42,34 @@
 		private $nameDB;
 
 		public function __construct(){
-
 			$this->usuario = parent::_USER_();
 			$this->password = parent::_PASS_();
 			$this->local = parent::_LOCAL_();
 			$this->nameDB = parent::_BD_();
-			$this->conectarDB();
 		}
 
-		protected function conectarDB(){
-			try{
-				$this->conex = new \PDO("mysql:host={$this->local};dbname={$this->nameDB}", $this->usuario , $this->password);
-			}catch (PDOException $e) {
-				print "¡Error!: " . $e->getMessage() . "<br/>";
-				die();
+	    private $pool = [];
+
+		public function getConnection(){
+
+			if(count($this->pool) > 0){
+			  return array_pop($this->pool);
+			} else {
+				$conn = new PDO("mysql:host={$this->local};dbname={$this->nameDB}", $this->usuario , $this->password); 
+			  return $conn;
 			}
+		
+		  }
+	  
+		public function release($conn){
+
+			$this->pool[] = $conn;
+		
 		}
- 	}
+	  
+	  }
  ?>
+
+
+
+
