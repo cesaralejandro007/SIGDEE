7f63d691d620129bdbbbd120ed34ad8b0efc9ce7
cesaralejandro007 <cesaralejandrovides2023@gmail.com>
2023-09-03 15:46:39 -0400

    cambios de pruebas

--- test/Censo/incluirCensoTest.php
+++ test/Censo/incluirCensoTest.php
@@ -11,20 +11,19 @@ class incluirCensoTest extends TestCase{
 
 	//1-Cuando el usuario Ingresa los datos correctamente
 	public function testCreacionCorrecta(){
-		$respuesta = $this->censo->incluir(45,"04-03-2023 21:43","05-03-2023 21:43","Nuevo Censo");
+		$respuesta = $this->censo->incluir(1,"02/09/2023 20:05","03/09/2023 20:06","Nuevo Censoss");
 		$this->assertEquals(1, $respuesta['resultado']);
 	}
 
-	//2-Cuando el usuario desea registrar un censo con la fecha de apertura mayor que la fecha de cierre
-	// o menor que la fecha actual
-	public function testNombreRepetido(){
-		$respuesta = $this->censo->incluir(45,"05-03-2023 21:43","04-03-2023 21:43","Nuevo Censo");
+	//2-Cuando el usuario desea registrar un censo con un usuario que no existe
+	public function testExisteUsuario(){
+		$respuesta = $this->censo->incluir(22523425,"02/09/2023 20:30","03/09/2023 20:05","Nuevo");
 		$this->assertEquals(3, $respuesta['resultado']);
 	}
 
-	//3-Cuando el usuario envia la descripción del censo con un valor que contiene caracteres especiales
+	//3-Cuando el usuario envia los datos del censo con valores que contiene caracteres especiales e incorrectos
 	public function testExpresiones(){
-		$respuesta = $this->censo->incluir(93,"04-03-2023 21:43","05-03-2023 21:43","Nuevo Censo***");
+		$respuesta = $this->censo->incluir(1,"asdasd","asdsadsa","Nuevo Cenas");
 		$this->assertEquals(2, $respuesta['resultado']);
 	}
 }
